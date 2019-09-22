ce3600cb767120c04395688147a98567570ffa2a
bliid <gh.bliid@gmail.com>
2019-09-22 16:48:12 +0100

    Updates

---
+++ precache-manifest.229bc8796fd70f62b5b4c2b4a2154025.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392c84c539e514a0a07bc870b83182e0",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "11c85c782384f0d1d69f",
+    "url": "/redux-todo/static/css/main.733bdbd8.chunk.css"
+  },
+  {
+    "revision": "be593eba61da30b47d35",
+    "url": "/redux-todo/static/js/2.9715c10f.chunk.js"
+  },
+  {
+    "revision": "11c85c782384f0d1d69f",
+    "url": "/redux-todo/static/js/main.a1275a72.chunk.js"
+  },
+  {
+    "revision": "85b70e0567957921b4a0",
+    "url": "/redux-todo/static/js/runtime-main.0082c3ca.js"
+  }
+]);
